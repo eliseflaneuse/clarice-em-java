2ed670001fd3982222ac305cf8d5d16e7a83048f
eliseflaneuse <eliseflaneuse@gmail.com>
2023-06-05 17:13:50 -0300

    First commit

---
+++ src/jogo/Personagem.java
@@ -0,0 +1,79 @@
+package jogo;
+
+public class Personagem {
+	
+	String nome;
+	int vida = 5; 
+	
+	
+	/*  CLARICE EM JAVA 
+	 * 
+	 *  python - cobrinha
+	 *  java - vida (café) ganhar ou perder ponto - abaixo de 0 o programa sai.
+	 *  javaScript - ação (efeitos especiais) 
+	 *  css - roupa (estilização)
+	 *  ruby - dinheiro (ruby on rails - dinheiro no trem - interceptar o trem?) 
+	 *  
+	 *  BSM - atributos 
+	 *  String proatividade = 4; 
+	 *  Categoria - front, back, full-stack 
+	 *  Cada categoria ganha mais pontos como herança;
+	 *  habilidades
+	 *  uma missão principal - 3 desafios (métodos)
+	 *  Podemos dar pontos no ínicio. 
+	 *  10 pontos 
+	 *  int inteligencia = 3;
+	 *  int destreza = 2;
+	 *  
+	 *  
+	 *  História, desafios lógicos no console - jogo de perguntas
+	 *  Atributos: int num = 10; if (se )num < 10 - condição 1, se num > 10 - condição 2
+	 *  
+	 *  DESAFIOS LÓGICOS (AUMENTANDO EM DIFICULDADE) E PERGUNTAS ENGRAÇADAS.
+	 *  pergunta lógica 
+	 *  cenário - perder ou ganhar vida 
+	 *  pergunta lógica 
+	 *  
+	 *  
+	 *  Esfinge - pergunta x ou y ? passa, ponto de dano - DADO!!! 6, 12, 20. 
+	 *  variável = ganho 2 pontos.
+	 *  
+	 *  Pergunta de programação - normalizer* - pergunta para passar da porta (meio do senhor dos anéis)
+	 *   com resposta escrita- palavra chave 
+	 *   
+	 *  Esquerda ou direita? (Gato da Alice)-inimigo ou não da pra avançar 
+	 *  
+	 *  loop infinito - game over. 
+	 *  
+	 *  Ideia da ray - viagem no tempo linear (do passado ao futuro)
+	 *  medieval - Mount Pyton 
+	 *  piratas 
+	 *  scifi apocaliptico 
+	 *  - mudar as falar no console de acordo com a época.
+	 *  - perguntas cada vez mais difíceis. 
+	 *  
+	 *  
+	 *  Começar em um cenário. 
+	 *  Personagens se repetem. 
+	 *  
+	 *  Narrativa - Story telling 
+	 *  +5 de proatividade = ajudar pessoa +vida (EVENTO)
+	 *  em outro momento - ponto pra distribuir nos atributos(perguntas técnicas)
+	 *  
+	 *  o grande if - Nível fácil, moderado ou difícil.
+	 *  (se modo hard - atributos trocados) 
+	 *  
+	 *  CARACTERÍSTICAS 
+	 *  Característica única que define o personagem que permanece, como cor ou vestimenta; 
+	 *  Coelho 
+	 *  Esfinge - Lagarta - oferece um trago
+	 *  Bobo da Corte  - Chapeleiro e a Lebre de Março
+	 *  Gato Azul Sorridente - oferece bebida (vc fica bebado e a pergunta fica + dificil) 
+	 *  
+	 *  Vilão Rainha de Copas (carta com 5 - pra representar os minutos) 
+	 *  
+	 *  Visual - Alice dentro da Xícara (Clarice em Java) 
+	 *  
+	 *  BOSS - Yuri o dono do tempo (+5 minutos) - Cortem as cabeças;
+	 */
+}
