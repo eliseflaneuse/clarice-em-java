10e7d50c49d734e6fadb597be420621d1be91922
eliseflaneuse <eliseflaneuse@gmail.com>
2023-06-07 09:52:28 -0300

    fixed game name and first drawing

--- src/game/Game.java
+++ src/game/Game.java
@@ -53,64 +53,7 @@ public class Game {
         scanner.close();
 	}
 	
-	private static void distributeAttributePoints(Player player, Scanner scanner) {
-        int remainingPoints = 10;
-
-        System.out.println("--- Distribua os Pontos de Atributo ---");
-
-        while (remainingPoints > 0) {
-            System.out.println("Pontos restantes: " + remainingPoints);
-            System.out.println("1. Responsabilidade Pessoal");
-            System.out.println("2. Mentalidade de Crescimento");
-            System.out.println("3. Proatividade");
-            System.out.println("4. Orientação ao Detalhe");
-            System.out.print("Escolha um atributo para distribuir os pontos (1-4): ");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
-
-            if (choice < 1 || choice > 4) {
-                System.out.println("Escolha inválida. Ta faltando orientação ao detalhe!");
-                continue;
-            }
-
-            System.out.print("Quantos pontos você quer alocar: ");
-            int points = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
-
-            if (points < 0 || points > remainingPoints) {
-                System.out.println("Invalid number of points. Please choose again.");// deixar engraçadinho
-                continue;
-            }
-
-            switch (choice) {
-                case 1:
-                    player.setResponsabilidadePessoal(points);
-                    break;
-                case 2:
-                    player.setMentalidadeDeCrescimento(points);
-                    break;
-                case 3:
-                    player.setProatividade(points);
-                    break;
-                case 4:
-                    player.setOrientacaoAoDetalhe(points);
-                    break;
-            }
-
-            remainingPoints -= points;
-        }
-        
-        // Deixar a ficha cute
-        System.out.println("\n\n--- Character Sheet ---");
-        System.out.println("Nome: " + player.getName());
-        System.out.println("Level: " + player.getLevel());
-        System.out.println("--- Distributed Attribute Points ---");
-        System.out.println("Responsabilidade Pessoal: " + player.getResponsabilidadePessoal());
-        System.out.println("Mentalidade de Crescimento: " + player.getMentalidadeDeCrescimento());
-        System.out.println("Proatividade: " + player.getProatividade());
-        System.out.println("Orientação ao Detalhe: " + player.getOrientacaoAoDetalhe() + "\n\n\n");
-    }
+	
 	private static void distributeAttributePoints(Player player, Scanner scanner) {
         int remainingPoints = 10;
 
@@ -174,12 +117,19 @@ public class Game {
         int messageLength = welcomeMessage.length();
 
         // Print top border
-        System.out.println("=========================================================================");
+        System.out.println("==============================================================================================");
 
         // Print message with padding
-        System.out.printf("%" + ((70 - messageLength) / 2 + messageLength) + "s%n", welcomeMessage);
-
+        //System.out.printf("%" + ((70 - messageLength) / 2 + messageLength) + "s%n", welcomeMessage);
+        System.out.println(" ____  _            _                                ___                  _                 _ ");
+        System.out.println("/   _\\| |          (_)                              |_  |                | |               | |");
+        System.out.println("| /  \\| | __ _ _ __ _  ___ ___    ___   _ __ ___      | | __ ___   ____ _| | __ _ _ __   __| |");
+        System.out.println("| |   | |/ _` | '__| |/ __/ _ \\  / _ ` | '_ ` _ \\     | |/ _` \\ \\ / / _` | |/ _` | '_ \\ / _` |");
+        System.out.println("| \\__/| | (_| | |  | | (_|  __/ |  _ /  | | | | | /\\__/ | (_| |\\ V / (_| | | (_| | | | | (_| |");
+        System.out.println(" \\____|_|\\__,_|_|  |_|\\___\\___|  \\___|  | |_| |_| \\____/ \\__,_| \\_/ \\__,_|_|\\__,_|_| |_|\\__,_|");
+        System.out.println("                                                                                                         ");
+        
         // Print bottom border
-        System.out.println("=========================================================================\n");
+        System.out.println("==============================================================================================\n");
     }
 }
